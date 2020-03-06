d322a3a8c1675f917982058f97068ed55f48ab9e
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2020-03-06 04:35:56 +0100

    Structurised episodes

--- routes/series/get/episodes.mjs
+++ routes/series/get/episodes.mjs
@@ -1,13 +1,79 @@
+import response from '../../predefined/responses.mjs';
+
 export default function getEpisodes(router, db) {
   router.get('/:language/:category/:serie/:season', (req, res) => {
     const { language, category, serie, season } = req.params;
-    console.log(language, category, serie, season);
-    try {
+    if (db.isSupportedLanguage(language)) {
+      if (db.structure[language].list().includes(category)) {
+        if (db.structure[language][category].list().includes(serie)) {
+          if (
+            db.structure[language][category][serie].season
+              .list()
+              .includes(season)
+          ) {
+            res.json(
+              response.ok({
+                episodes: db.structure[language][category][serie].season[
+                  season
+                ].episode.list()
+              })
+            );
+          } else {
+            res.json(
+              response.error.unknownField(
+                { index: 4, value: season },
+                ['series', language, category, serie, season],
+                {
+                  existing: {
+                    seasons: db.structure[language][category][
+                      serie
+                    ].season.list()
+                  }
+                },
+                'Season does not exist.'
+              )
+            );
+          }
+        } else {
+          res.json(
+            response.error.unknownField(
+              { index: 3, value: serie },
+              ['series', language, category, serie, season],
+              {
+                existing: {
+                  series: db.structure[language][category].list()
+                }
+              },
+              'Serie does not exist.'
+            )
+          );
+        }
+      } else {
+        res.json(
+          response.error.unknownField(
+            { index: 2, value: category },
+            ['series', language, category, serie, season],
+            { existing: { categories: db.structure[language].list() } },
+            'Category does not exist.'
+          )
+        );
+      }
+    } else {
       res.json(
-        db.structure[language][category][serie].season[season].episode.list()
+        response.error.unknownField(
+          { index: 1, value: language },
+          ['series', language, category, serie, season],
+          { existing: { languages: db.structure.languages() } },
+          'Language does not exist.'
+        )
       );
-    } catch (err) {
-      console.error(err);
     }
+    // try {
+    //   res.json(
+    //     db.structure[language][category][serie].season[season].episode.list()
+    //   );
+    // } catch (err) {
+    //   console.error(err);
+    // }
   });
 }
