a5b2d26b29783e408dc9fb5273d36051d4f8955c
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2020-03-06 03:54:59 +0100

    updated categories response

--- routes/series/get/categories.mjs
+++ routes/series/get/categories.mjs
@@ -1,11 +1,28 @@
+import response from '../../predefined/responses.mjs';
 export default function getCategories(router, db) {
   router.get('/:language', (req, res) => {
     const { language } = req.params;
-    try {
-      const categories = db.structure[language].list();
-      res.json(categories);
-    } catch (err) {
-      console.error(err);
+    if (db.isSupportedLanguage(language)) {
+      try {
+        const categories = db.structure[language].list();
+        res.json(response.ok({ categories }));
+      } catch (err) {
+        const structuredResponse = response.error.unknown(
+          { index: 1, value: language },
+          ['series', language]
+        );
+        console.error(err, structuredResponse);
+        res.json(structuredResponse);
+      }
+    } else {
+      res.json(
+        response.error.unknownField(
+          { index: 1, value: language },
+          ['series', language],
+          { existing: { languages: db.structure.languages() } },
+          'Language does not exist.'
+        )
+      );
     }
   });
 }
