3ed2acbf232288d3082c5ac51ab128f60b4ec908
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2020-03-06 04:38:10 +0100

    added missing values to response attribute fields

--- routes/series/get/seasons.mjs
+++ routes/series/get/seasons.mjs
@@ -29,7 +29,7 @@ export default function getSeasons(router, db) {
         res.json(
           response.error.unknownField(
             { index: 2, value: category },
-            ['series', language, category],
+            ['series', language, category, serie],
             { existing: { categories: db.structure[language].list() } },
             'Category does not exist.'
           )
@@ -39,32 +39,11 @@ export default function getSeasons(router, db) {
       res.json(
         response.error.unknownField(
           { index: 1, value: language },
-          ['series', language, category],
+          ['series', language, category, serie],
           { existing: { languages: db.structure.languages() } },
           'Language does not exist.'
         )
       );
     }
-    // if (!db.structure[language][category]) {
-    //   res.json({
-    //     error: `category missing`,
-    //     category,
-    //     categories: db.structure.list()
-    //   });
-    //   return;
-    // }
-    // if (!db.structure[language][category][serie]) {
-    //   res.json({
-    //     error: `serie missing`,
-    //     serie,
-    //     series: db.structure[language][category].list()
-    //   });
-    //   return;
-    // }
-    // try {
-    //   res.json(db.structure[language][category][serie].season.list());
-    // } catch (err) {
-    //   console.error(err);
-    // }
   });
 }
