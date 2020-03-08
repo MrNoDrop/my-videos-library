4b4e1556fc39f559cc567237a703a0720d098e0c
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2020-03-08 02:45:36 +0100

    Created an change location button component

---
+++ client/src/components/button/change/location.jsx
@@ -0,0 +1,188 @@
+import React, { useRef, useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { push } from 'redux-first-routing';
+import { connect } from 'react-redux';
+import './location.scss';
+import image from '../../../images';
+import Hover from '../../hover';
+
+const mapDispatchToProps = dispatch => ({
+  changeLocation: pathname => {
+    pathname.endsWith('/') &&
+      (pathname = pathname.substring(0, pathname.length - 1));
+    document.location.pathname !== pathname && dispatch(push(pathname));
+  }
+});
+
+export const supportedViewmodes = {
+  list: 'list',
+  horizontal: 'horizontal',
+  vertical: 'vertical'
+};
+function ChangeLocationButton({
+  image: { horizontal, vertical },
+  parentScrollEventCounter,
+  href,
+  style,
+  viewmode,
+  hovertext,
+  parentRef,
+  changeLocation,
+  children,
+  className,
+  ...other
+}) {
+  const {
+    hidden: hovertextHidden,
+    mousePostion,
+    onMouseEnter,
+    onMouseLeave,
+    onMouseMove
+  } = Hover.useHide();
+  const { ref, render } = useRender(parentRef, parentScrollEventCounter);
+  const [horizontalTrigger, horizontalImageLoaded] = useLoadImage(
+    render,
+    horizontal
+  );
+  const [verticalTrigger, verticalImageLoaded] = useLoadImage(render, vertical);
+  const hover = hovertext && (
+    <Hover hidden={hovertextHidden} {...{ mousePostion }}>
+      {hovertext}
+    </Hover>
+  );
+  switch (viewmode) {
+    default:
+      return [
+        hover,
+        <div
+          key="undefined"
+          onClick={() => changeLocation(href)}
+          {...{ ...other, className }}
+        >
+          {children}
+        </div>
+      ];
+
+    case supportedViewmodes.list:
+      return [
+        hover,
+        <button
+          key="list"
+          className={`change-location-button${
+            className ? ' ' + className : ''
+          }`}
+          onClick={() => changeLocation(href)}
+          {...{ ...other, onMouseEnter, onMouseLeave, onMouseMove }}
+        >
+          {children}
+        </button>
+      ];
+    case supportedViewmodes.horizontal:
+      return [
+        horizontalTrigger,
+        hover,
+        <div
+          key="horizontal"
+          {...{
+            ref,
+            onMouseEnter,
+            onMouseLeave,
+            onMouseMove,
+            ...other,
+            style: {
+              ...style,
+              backgroundImage: `url(${
+                horizontalImageLoaded ? horizontal : image.animated.loading
+              })`
+            }
+          }}
+          onClick={() => changeLocation(href)}
+          className={`change-location-image-button horizontal${
+            className ? ' ' + className : ''
+          }`}
+        >
+          {children}
+        </div>
+      ];
+    case supportedViewmodes.vertical:
+      return [
+        verticalTrigger,
+        hover,
+        <div
+          key="vertical"
+          {...{
+            ref,
+            onMouseEnter,
+            onMouseLeave,
+            onMouseMove,
+            ...other,
+            style: {
+              ...style,
+              backgroundImage: `url(${
+                verticalImageLoaded ? vertical : image.animated.loading
+              })`
+            }
+          }}
+          onClick={() => changeLocation(href)}
+          className={`change-location-image-button vertical${
+            className ? ' ' + className : ''
+          }`}
+        >
+          {children}
+        </div>
+      ];
+  }
+}
+
+export default connect(null, mapDispatchToProps)(ChangeLocationButton);
+
+function useRender(parentRef, trigger) {
+  const ref = useRef();
+  const [render, setRender] = useState(false);
+  const [firstRun, setFirstRun] = useState(true);
+
+  useEffect(() => {
+    if (ref.current) {
+      if (firstRun) {
+        setFirstRun(false);
+        return;
+      }
+      const rect = ReactDOM.findDOMNode(ref.current).getBoundingClientRect();
+      const parentRect = ReactDOM.findDOMNode(
+        parentRef.current
+      ).getBoundingClientRect();
+      if (
+        rect.left < parentRect.right &&
+        rect.right > parentRect.left &&
+        rect.top < parentRect.bottom &&
+        rect.bottom > parentRect.top &&
+        !render
+      ) {
+        setRender(true);
+      }
+    }
+  }, [ref, parentRef, render, setRender, firstRun, trigger]);
+  return { ref, render };
+}
+
+function useLoadImage(render, image) {
+  const [loadingImage, setLoadingImage] = useState(undefined);
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    if (render && !loaded && !loadingImage) {
+      setLoadingImage(
+        <img
+          key={image}
+          src={image}
+          style={{ width: 0, height: 0 }}
+          alt={''}
+          onLoad={() => {
+            setLoaded(true);
+            setLoadingImage(undefined);
+          }}
+        />
+      );
+    }
+  }, [render, loadingImage, setLoadingImage, setLoaded]);
+  return [loadingImage, loaded];
+}
