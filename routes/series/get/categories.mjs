a1b3db107fd45cf4e2a10ceffad2df70df159142
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2020-03-08 07:20:11 +0100

    added category language mapping

--- routes/series/get/categories.mjs
+++ routes/series/get/categories.mjs
@@ -1,28 +1,15 @@
 import response from '../../predefined/responses.mjs';
+import check from '../check/get.mjs';
+
 export default function getCategories(router, db) {
-  router.get('/:language', (req, res) => {
-    const { language } = req.params;
-    if (db.isSupportedLanguage(language)) {
-      try {
-        const categories = db.structure[language].list();
-        res.json(response.ok({ categories }));
-      } catch (err) {
-        const structuredResponse = response.error.unknown(
-          { index: 1, value: language },
-          ['series', language]
-        );
-        console.error(err, structuredResponse);
-        res.json(structuredResponse);
-      }
-    } else {
-      res.json(
-        response.error.unknownField(
-          { index: 1, value: language },
-          ['series', language],
-          { existing: { languages: db.structure.languages() } },
-          'Language does not exist.'
-        )
-      );
+  router.get(
+    '/:language',
+    check.preconfiguration,
+    check.language.bind(this, db),
+    (req, res) => {
+      const { language } = req.params;
+      const categories = db.structure[language].list();
+      res.json(response.ok({ path: ['series', language], categories }));
     }
-  });
+  );
 }
