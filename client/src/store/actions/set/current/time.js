f963eceae8649156fa96fd7b5bb0483a4067547c
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2020-03-11 14:08:38 +0100

    Fixed setCurrentTime action

--- client/src/store/actions/set/current/time.js
+++ client/src/store/actions/set/current/time.js
@@ -1,7 +1,7 @@
-// import SET_CURRENT_TIME from '../../types';
+import { SET_CURRENT_TIME } from '../../types';
 
 export default (currenttime = {}) => ({
-  type: 'SET_CURRENT_TIME',
+  type: SET_CURRENT_TIME,
   payload: {
     currenttime: { ...currenttime }
   }
