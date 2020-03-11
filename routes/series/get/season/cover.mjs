7b2daced8d89fd6a4109dd8179ffe61c805b486f
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2020-03-11 14:05:40 +0100

    Removed console log

--- routes/series/get/season/cover.mjs
+++ routes/series/get/season/cover.mjs
@@ -3,9 +3,6 @@ export default function getSeasonCover(router, db) {
     '/shared/:category/:serie/:season/cover/:orientation',
     async (req, res) => {
       const { category, serie, season, orientation } = req.params;
-      console.log(
-        db.structure.shared[category][serie].season[season].cover[orientation]
-      );
       try {
         res.sendFile(
           db.structure.shared[category][serie].season[season].cover[
