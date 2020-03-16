d9d642f862fa019ad1f463a971bf67799550d36b
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2020-03-16 02:01:36 +0100

    Made a Subtitles Component and moved some logic from Subtitle Component there

---
+++ client/src/components/subtitle.jsx
@@ -0,0 +1,77 @@
+import React, { useRef, useEffect, useState } from 'react';
+import './subtitle.scss';
+
+function Subtitle({
+  videoTime,
+  collection,
+  className,
+  style,
+  onWheel,
+  ...props
+}) {
+  const ref = useRef();
+  const [fontSize, setFontSize] = useState(4);
+  const changeFontSize = event => {
+    let delta;
+
+    if (event.wheelDelta) {
+      delta = event.wheelDelta;
+    } else {
+      delta = -1 * event.deltaY;
+    }
+
+    if (delta > 0) {
+      setFontSize(fontSize - 0.1 < 2.5 ? 2.5 : fontSize - 0.1);
+    } else if (delta < 0) {
+      setFontSize(fontSize + 0.1 > 5 ? 5 : fontSize + 0.1);
+    }
+  };
+  const currentSubtitle = useDetermineCurrentSubtitle(videoTime, collection);
+  return (
+    <div
+      {...{
+        ref,
+        className: `subtitle${className ? ` ${className}` : ''}`,
+        style: {
+          ...style,
+          fontSize: `${fontSize}vmin`,
+          textShadow: `-${fontSize / 25}vmin -${fontSize /
+            25}vmin 0 white, ${fontSize / 25}vmin -${fontSize / 25}vmin 0 white,
+          -${fontSize / 25}vmin ${fontSize / 25}vmin 0 white, ${fontSize /
+            25}vmin ${fontSize / 25}vmin 0 white`
+        },
+        ...props
+      }}
+      hidden={!currentSubtitle}
+      scrollable
+      onWheel={event => {
+        changeFontSize(event);
+        if (typeof onWheel === 'function') {
+          onWheel(event);
+        }
+      }}
+    >
+      {currentSubtitle}
+    </div>
+  );
+}
+export default Subtitle;
+
+function useDetermineCurrentSubtitle(videoTime, collection) {
+  const [currentSubtitle, setCurrentSubtitle] = useState(undefined);
+  useEffect(() => {
+    let newCurrentSubtitle = null;
+    if (typeof collection === 'object') {
+      for (let [start, end, subtitle] of Object.values(collection)) {
+        if (videoTime >= start && videoTime <= end) {
+          newCurrentSubtitle = subtitle;
+          break;
+        }
+      }
+    }
+    if (newCurrentSubtitle !== currentSubtitle) {
+      setCurrentSubtitle(newCurrentSubtitle);
+    }
+  }, [videoTime, currentSubtitle, setCurrentSubtitle]);
+  return currentSubtitle;
+}
--- client/src/components/subtitles.jsx
+++ client/src/components/subtitles.jsx
@@ -1,149 +1,59 @@
-import React, { useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import addSubtitles from '../store/actions/add/subtitles';
-import setSelectedSubtitles from '../store/actions/set/selected/subtitles';
-import { connect } from 'react-redux';
 import parseSrt from './tools/parseSrt.mjs';
+import { connect } from 'react-redux';
+import Subtitle from './subtitle';
+import './subtitles.scss';
+
 const mapStateToProps = ({
-  state: { 'selected-subtitles': selectedSubtitles, subtitles }
-}) => ({
-  availableSubtitles: subtitles,
-  selectedSubtitles
-});
+  state: { subtitles, 'selected-subtitles': selected }
+}) => ({ fetched: subtitles, selected });
 
 const mapDispatchToProps = dispatch => ({
-  addSubtitles: (availableSubtitles, newSubtitles) =>
-    dispatch(addSubtitles(availableSubtitles, newSubtitles)),
-  setSelectedSubtitles: selectedSubtitles =>
-    dispatch(setSelectedSubtitles(selectedSubtitles))
+  add: (fetched, newSubtitles) => dispatch(addSubtitles(fetched, newSubtitles))
 });
 
 function Subtitles({
-  videoRef,
-  addSubtitles,
-  availableSubtitles,
+  className,
+  videoTime,
   subtitles = {},
-  current,
-  className = 'subtitles',
-  style,
-  onWheel,
-  selectedSubtitles,
-  ...props
+  fetched,
+  selected,
+  add
 }) {
-  delete props.setSelectedSubtitles;
-  const [fontSize, setFontSize] = useState(4);
-  const changeFontSize = event => {
-    let delta;
-
-    if (event.wheelDelta) {
-      delta = event.wheelDelta;
-    } else {
-      delta = -1 * event.deltaY;
-    }
-
-    if (delta > 0) {
-      setFontSize(fontSize - 0.1 < 2.5 ? 2.5 : fontSize - 0.1);
-    } else if (delta < 0) {
-      setFontSize(fontSize + 0.1 > 5 ? 5 : fontSize + 0.1);
-    }
-  };
-  useFetchSubtitles(
-    subtitles,
-    availableSubtitles,
-    addSubtitles,
-    selectedSubtitles
-  );
-  const videoTime = useDetermineVideoTime(videoRef);
-  const currentSubtitle = useDetermineCurrentSubtitle(
-    current,
-    videoTime,
-    subtitles,
-    availableSubtitles
-  );
+  const ref = useRef();
+  useFetchSubtitles(subtitles, fetched, add, selected);
   return (
     <div
-      {...{
-        className,
-        style: {
-          ...style,
-          fontSize: `${fontSize}vmin`,
-          textShadow: `-${fontSize / 25}vmin -${fontSize /
-            25}vmin 0 white, ${fontSize / 25}vmin -${fontSize / 25}vmin 0 white,
-          -${fontSize / 25}vmin ${fontSize / 25}vmin 0 white, ${fontSize /
-            25}vmin ${fontSize / 25}vmin 0 white`
-        },
-        ...props
-      }}
-      hidden={!currentSubtitle}
-      scrollable
-      onWheel={event => {
-        changeFontSize(event);
-        if (typeof onWheel === 'function') {
-          onWheel(event);
-        }
-      }}
+      {...{ ref, className: `subtitles${className ? ` ${className}` : ''}` }}
     >
-      {currentSubtitle}
+      {selected.map(selected => (
+        <Subtitle
+          {...{
+            key: selected,
+            videoTime,
+            collection: fetched[subtitles[selected]]
+          }}
+        />
+      ))}
     </div>
   );
 }
 
-function SubtitlesButton({
-  subtitles,
-  selectedSubtitles,
-  availableSubtitles,
-  setSelectedSubtitles,
-  ...other
-}) {
-  return [
-    <div className="available-subtitles">
-      {Object.keys(subtitles).map(language => (
-        <div
-          key={language}
-          className={`available-subtitle${
-            selectedSubtitles.includes(language) ? ' selected' : ''
-          }`}
-          onClick={() =>
-            selectedSubtitles.includes(language)
-              ? setSelectedSubtitles([
-                  ...selectedSubtitles.filter(selected => language !== selected)
-                ])
-              : setSelectedSubtitles([...selectedSubtitles, language])
-          }
-        >
-          {language}
-        </div>
-      ))}
-    </div>,
-    <div className="subtitles-button">Subtitles</div>
-  ];
-}
-
-Subtitles.Button = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(SubtitlesButton);
 export default connect(mapStateToProps, mapDispatchToProps)(Subtitles);
 
-function useFetchSubtitles(
-  subtitles,
-  availableSubtitles,
-  addSubtitles,
-  selectedSubtitles
-) {
+function useFetchSubtitles(subtitles, fetched, add, selected) {
   const [fetching, setFetching] = useState(false);
   useEffect(() => {
     if (!fetching) {
       setFetching(true);
       (async () => {
-        const freshlyAvailableSubtitles = {};
+        const freshSubtitles = {};
         for (let subset in subtitles) {
-          if (
-            availableSubtitles[subtitles[subset]] ||
-            !selectedSubtitles.includes(subset)
-          ) {
+          if (fetched[subtitles[subset]] || !selected.includes(subset)) {
             continue;
           }
-          freshlyAvailableSubtitles[subtitles[subset]] = parseSrt(
+          freshSubtitles[subtitles[subset]] = parseSrt(
             (
               await (await (await fetch(subtitles[subset])).body)
                 .getReader()
@@ -151,65 +61,11 @@ function useFetchSubtitles(
             ).value
           );
         }
-        if (Object.keys(freshlyAvailableSubtitles).length > 0) {
-          addSubtitles(availableSubtitles, freshlyAvailableSubtitles);
+        if (Object.keys(freshSubtitles).length > 0) {
+          add(fetched, freshSubtitles);
         }
         setFetching(false);
       })();
     }
-  }, [subtitles, availableSubtitles, addSubtitles, fetching, setFetching]);
-}
-
-function useDetermineVideoTime(videoRef) {
-  const [timeouttime, setTimeouttime] = useState(undefined);
-  const [videoTime, setVideoTime] = useState(
-    (videoRef && videoRef.current && videoRef.current.currentTime) || 0
-  );
-  useEffect(() => {
-    if (!timeouttime) {
-      setTimeouttime(
-        setTimeout(() => {
-          if (videoRef && videoRef.current) {
-            if (videoTime !== videoRef.current.currentTime) {
-              setVideoTime(videoRef.current.currentTime);
-            }
-          }
-          setTimeouttime(clearTimeout(timeouttime));
-        }, 50)
-      );
-    }
-  }, [videoRef, timeouttime, setTimeouttime, videoTime, setVideoTime]);
-  return videoTime;
-}
-
-function useDetermineCurrentSubtitle(
-  current,
-  videoTime,
-  subtitles,
-  availableSubtitles
-) {
-  const [currentSubtitle, setCurrentSubtitle] = useState(undefined);
-  useEffect(() => {
-    let newCurrentSubtitle = null;
-    if (availableSubtitles[subtitles[current]]) {
-      for (let [start, end, subtitle] of availableSubtitles[
-        subtitles[current]
-      ]) {
-        if (videoTime >= start && videoTime <= end) {
-          newCurrentSubtitle = subtitle;
-          break;
-        }
-      }
-    }
-    if (newCurrentSubtitle !== currentSubtitle) {
-      setCurrentSubtitle(newCurrentSubtitle);
-    }
-  }, [
-    videoTime,
-    current,
-    currentSubtitle,
-    setCurrentSubtitle,
-    availableSubtitles
-  ]);
-  return currentSubtitle;
+  }, [subtitles, fetched, add, fetching, setFetching]);
 }
