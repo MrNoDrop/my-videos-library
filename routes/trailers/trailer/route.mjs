967cf0fd7b32d7eed9d1a94c90b07fad4c05436c
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2024-05-29 03:25:30 +0200

    Get trailer route is finished!

--- routes/trailers/trailer/route.mjs
+++ routes/trailers/trailer/route.mjs
@@ -7,9 +7,28 @@ export default function getTrailerRoute(router, moviesDB, seriesDB) {
   router.get("/trailer/route/:language", (req, res) => {
     const { language } = req.params;
     const { definedRoutes } = req.query;
-    const definedTrailerRoutes = JSON.parse(definedRoutes || "[]");
+    const definedTrailerRoutes = JSON.parse(
+      JSON.parse(definedRoutes || '"[]"')
+    );
     let trailerRoute = null;
+    let allTrailers = 0;
+    seriesDB.structure[language].list().forEach((category) => {
+      const trailers = seriesDB.structure[language][category].list();
+      allTrailers += 1 * trailers.length;
+    });
+    moviesDB.structure[language].list().forEach((category) => {
+      const trailers = moviesDB.structure[language][category].list();
+      allTrailers += 1 * trailers.length;
+    });
     do {
+      console.log(
+        typeof definedTrailerRoutes,
+        definedTrailerRoutes.length,
+        allTrailers
+      );
+      if (definedTrailerRoutes.length === allTrailers) {
+        break;
+      }
       let trailersDB = undefined;
       let trailerRoutePrefix = undefined;
       switch (Math.floor(Math.random() * 2)) {
@@ -30,7 +49,7 @@ export default function getTrailerRoute(router, moviesDB, seriesDB) {
       const trailers = trailersDB.structure[language][category].list();
       const trailer = trailers[Math.floor(Math.random() * trailers.length)];
       trailerRoute = `${trailerRoutePrefix}/${language}/${category}/${trailer}/trailer`;
-    } while (definedTrailerRoutes.includes(trailerRoute) /*|| undefined*/);
+    } while (definedTrailerRoutes.includes(trailerRoute));
     res.json(
       response.ok({
         path: ["trailers", "trailer", "route", language],
