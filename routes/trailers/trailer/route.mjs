231a859355de094f2d1f3d865616b0745176e4eb
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2024-05-29 02:34:32 +0200

    woring on "get" trailer route. A lot of work left.

--- routes/trailers/trailer/route.mjs
+++ routes/trailers/trailer/route.mjs
@@ -7,31 +7,38 @@ export default function getTrailerRoute(router, moviesDB, seriesDB) {
   router.get("/trailer/route/:language", (req, res) => {
     const { language } = req.params;
     const { definedRoutes } = req.query;
-    let trailerRoute = -1;
-    const definedTrailers = JSON.parse(definedRoutes ? definedRoutes : "[]");
-    let trailersDB = undefined;
-    switch (Math.floor(Math.random() * 2)) {
-      case SERIES:
-        trailersDB = seriesDB;
-        break;
-      case MOVIES:
-        trailersDB = moviesDB;
-        break;
-      default:
-        throw new Error();
-    }
-    const categories = trailersDB.structure[language].list();
-    const category =
-      categories[Math.floor(Math.random() * (categories.length + 1))];
-    const trailers = trailersDB.structure[language][category].list();
-
-    console.log("categories", categories.length, "category", category, "test:");
-    if (!definedTrailers.includes(trailer)) {
-      trailerRoute = trailer;
-    }
+    const definedTrailerRoutes = JSON.parse(
+      definedRoutes ? definedRoutes : "[]"
+    );
+    let trailerRoute = null;
+    do {
+      let trailersDB = undefined;
+      let trailerRoutePrefix = undefined;
+      switch (Math.floor(Math.random() * 2)) {
+        case SERIES:
+          trailerRoutePrefix = "/series";
+          trailersDB = seriesDB;
+          break;
+        case MOVIES:
+          trailerRoutePrefix = "/movies";
+          trailersDB = moviesDB;
+          break;
+        default:
+          throw new Error();
+      }
+      const categories = trailersDB.structure[language].list();
+      const category =
+        categories[Math.floor(Math.random() * categories.length)];
+      const trailers = trailersDB.structure[language][category].list();
+      const trailer = trailers[Math.floor(Math.random() * trailers.length)];
+      trailerRoute = `${trailerRoutePrefix}/${language}/${category}/${trailer}/trailer`;
+      if (!definedTrailerRoutes.icludes(trailerRoute)) {
+        definedTrailerRoutes.push(trailerRoute);
+      }
+    } while (!definedTrailerRoutes.includes(trailerRoute));
     res.json(
       response.ok({
-        path: ["trailers", language, "trailer", "route"],
+        path: ["trailers", "trailer", "route", language],
         trailerRoute,
       })
     );
