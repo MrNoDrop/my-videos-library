6a7d60dff08bdccd95a92c512e89f24961eb01a8
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2024-06-01 18:52:13 +0200

    bugfix

--- client/src/components/button/viewmode.jsx
+++ client/src/components/button/viewmode.jsx
@@ -1,19 +1,19 @@
-import React, { useRef } from 'react';
-import { connect } from 'react-redux';
-import images from '../../images';
-import setViewmode from '../../store/actions/set/viewmode';
-import './viewmode.scss';
+import React, { useRef } from "react";
+import { connect } from "react-redux";
+import images from "../../images";
+import setViewmode from "../../store/actions/set/viewmode";
+import "./viewmode.scss";
 
 const mapStateToProps = ({ state: { viewmode } }) => ({
-  currentViewmode: viewmode
+  currentViewmode: viewmode,
 });
-const mapDispatchToProps = dispatch => ({
-  setViewmode: viewmode => dispatch(setViewmode(viewmode))
+const mapDispatchToProps = (dispatch) => ({
+  setViewmode: (viewmode) => dispatch(setViewmode(viewmode)),
 });
 export const viewmodes = {
-  list: 'list',
-  horizontal: 'horizontal',
-  vertical: 'vertical'
+  list: "list",
+  horizontal: "horizontal",
+  vertical: "vertical",
 };
 const allViewmodes = (() => Object.values(viewmodes))();
 function ButtonViewmode({
@@ -25,50 +25,53 @@ function ButtonViewmode({
 }) {
   const ref = useRef();
   for (let key in alterSelected) {
-    if (typeof alterSelected[key] !== 'object') {
+    if (typeof alterSelected[key] !== "object") {
       alterSelected[key] = [alterSelected[key]];
-    } else if (alterSelected[key].constructor.name !== 'Array') {
+    } else if (alterSelected[key].constructor.name !== "Array") {
       alterSelected[key] = Object.values(alterSelected[key]);
     }
   }
   return (
     <div className="view-modes" {...{ ref, ...other }}>
-      {viewmodes.map(viewmode => {
+      {viewmodes.map((viewmode) => {
         switch (viewmode) {
           default:
-            return '';
-          case 'list':
+            return "";
+          case "list":
             return (
               <images.interactive.viewmode.List
+                key="list"
                 className="viewmode"
                 enabled={
                   alterSelected.list
                     ? alterSelected.list.includes(currentViewmode)
-                    : currentViewmode === 'list'
+                    : currentViewmode === "list"
                 }
                 onClick={() => setViewmode(viewmode)}
               />
             );
-          case 'horizontal':
+          case "horizontal":
             return (
               <images.interactive.viewmode.Horizontal
+                key="horizontal"
                 className="viewmode"
                 enabled={
                   alterSelected.horizontal
                     ? alterSelected.horizontal.includes(currentViewmode)
-                    : currentViewmode === 'horizontal'
+                    : currentViewmode === "horizontal"
                 }
                 onClick={() => setViewmode(viewmode)}
               />
             );
-          case 'vertical':
+          case "vertical":
             return (
               <images.interactive.viewmode.Vertical
+                key="vertical"
                 className="viewmode"
                 enabled={
                   alterSelected.vertical
                     ? alterSelected.vertical.includes(currentViewmode)
-                    : currentViewmode === 'vertical'
+                    : currentViewmode === "vertical"
                 }
                 onClick={() => setViewmode(viewmode)}
               />
