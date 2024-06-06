2bb292f2c630a92ea7159f85e8b657fea89798d9
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2024-06-06 21:38:21 +0200

    Update home path icon.

--- client/src/components/menu/index.js
+++ client/src/components/menu/index.js
@@ -1,54 +1,54 @@
-import React from 'react';
-import { push } from 'redux-first-routing';
-import { connect } from 'react-redux';
+import React from "react";
+import { push } from "redux-first-routing";
+import { connect } from "react-redux";
 
-import MenuHeader from './header';
-import MenuFooter, { Entry } from './footer';
-import OfflineNotice from '../screen/notice/noInternetConnection';
-import images from '../../images';
+import MenuHeader from "./header";
+import MenuFooter, { Entry } from "./footer";
+import OfflineNotice from "../screen/notice/noInternetConnection";
+import images from "../../images";
 
 const mapStateToProps = ({
   state: {
     navigator: { online },
-    user: { language, name }
+    user: { language, name },
   },
-  router
+  router,
 }) => ({ language, userName: name, router, isOnLine: online });
-const mapDispatchToProps = dispatch => ({
-  changePath: path => dispatch(push(path))
+const mapDispatchToProps = (dispatch) => ({
+  changePath: (path) => dispatch(push(path)),
 });
 const appTitle = {
-  pl: 'Filmowo',
-  en: 'Filmically',
-  nl: 'Filmisch',
-  fr: 'Filmiquement'
+  pl: "Filmowo",
+  en: "Filmically",
+  nl: "Filmisch",
+  fr: "Filmiquement",
 };
 function Menu({ language, router, changePath, isOnLine, userName }) {
   const { routes } = router;
   const path = {
     series: `/${language}/${routes[language].series}`,
     movies: `/${language}/${routes[language].movies}`,
-    home: `/${language}/${routes[language].home}`
+    home: `/${language}/${routes[language].home}`,
   };
   document.title = appTitle[language];
   return [
     <OfflineNotice
-      {...{ key: 'offline-notice', language, isOnLine, userName }}
+      {...{ key: "offline-notice", language, isOnLine, userName }}
     />,
     <MenuHeader
       {...{
-        key: 'header',
+        key: "header",
         title: appTitle[language],
         image: { src: images.menu.hd, alt: `${appTitle[language]} HD` },
-        changePath
+        changePath,
       }}
     />,
-    <MenuFooter {...{ key: 'footer', changePath }}>
+    <MenuFooter {...{ key: "footer", changePath }}>
       <Entry
         title={routes[language].series}
         image={{
           src: <images.interactive.menu.List />,
-          alt: 'series'
+          alt: "series",
         }}
         enabled={router.pathname.includes(path.series)}
         href={path.series}
@@ -56,8 +56,8 @@ function Menu({ language, router, changePath, isOnLine, userName }) {
       <Entry
         title={routes[language].home}
         image={{
-          src: <images.interactive.menu.Stats />,
-          alt: 'home'
+          src: <images.interactive.menu.Home />,
+          alt: "home",
         }}
         enabled={router.pathname.includes(path.home)}
         href={path.home}
@@ -66,12 +66,12 @@ function Menu({ language, router, changePath, isOnLine, userName }) {
         title={routes[language].movies}
         image={{
           src: <images.interactive.menu.Stats />,
-          alt: 'movies'
+          alt: "movies",
         }}
         enabled={router.pathname.includes(path.movies)}
         href={path.movies}
       />
-    </MenuFooter>
+    </MenuFooter>,
   ];
 }
 
--- client/src/images/interactive/menu/index.js
+++ client/src/images/interactive/menu/index.js
@@ -1,3 +1,4 @@
 export { default as Stats } from "./stats";
 export { default as List } from "./list";
 export { default as Fork } from "./fork";
+export { default as Home } from "./home";
